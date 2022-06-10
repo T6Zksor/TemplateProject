45c772f829f95a25641f87b5f29a06ffdb2ef877
t6zksor <t6zksor@gmail.com>
2022-06-11 00:16:02 +0800

    specify generator

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ if (!(Test-Path -Path $BuildOut)) {
 Push-Location -Path $BuildOut
 Write-Host "current directory $BuildOut"
 
-cmake .. -A Win32 -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build .
 
 Write-Host "start running..."

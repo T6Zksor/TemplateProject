45240160b103e70ab831bff4cc26eba6d8344015
t6zksor <t6zksor@gmail.com>
2022-06-18 00:37:42 +0800

    let it depend

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,11 @@
+#include "zlib.h"
+#include "uv.h"
+
 #include <iostream>
 
 int main() {
     std::cout << "hello" << std::endl;
+    std::cout << zlibVersion() << std::endl;
+    std::cout << uv_version() << std::endl;
     return 0;
 }

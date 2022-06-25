25b65de31576cef1e32420715c7cd1c7cbbacb67
t6zksor <t6zksor@gmail.com>
2022-06-25 14:14:29 +0800

    a qt project, wip

--- build.ps1
+++ build.ps1
@@ -8,14 +8,14 @@ Write-Host "current directory $BuildOut"
 
 #-DVCPKG_TARGET_TRIPLET=x64-windows-static
 #-DCMAKE_PREFIX_PATH="path/to/Qt5/lib/cmake"
-cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH="D:\\Qt\\Qt5.12.1\\5.12.1\\msvc2017_64\\lib\\cmake" -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH="D:\\Qt\\5.15.2\\msvc2019_64\\lib\\cmake" -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/vcpkg/scripts/buildsystems/vcpkg.cmake"
 cmake --build .
 
 if ($LastExitCode -eq 0) {
     Write-Host "start running..."
     Write-Host "#############################################################################"
 
-    ./src/Debug/main.exe
+    #./src/Debug/main.exe
 }
 
 Pop-Location
